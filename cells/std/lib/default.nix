ca0fa1577d9ff4e1bf62c74d3482cd0e1ccf9596
David Arnold <david.arnold@iohk.io>
2022-03-07 23:16:36 -0500

    fix(std/lib): fromMakesWith adapt & deduce system

--- cells/std/lib/default.nix
+++ cells/std/lib/default.nix
@@ -5,27 +5,34 @@ let
   nixpkgs = inputs.nixpkgs;
 in
 {
-  fromMakesWith = inputs': system': let
+  fromMakesWith = inputs': let
     inputsChecked =
       assert nixpkgs.lib.assertMsg (builtins.hasAttr "makes" inputs') (
         nixpkgs.lib.traceSeqN 1 inputs' ''
 
-          In order to be able to use 'std.lib.<system>.std-fromMakesWith', an input
+          In order to be able to use 'std.std.lib.fromMakesWith', an input
           named 'makes' must be defined in the flake. See inputs above.
+        ''
+      );
+      assert nixpkgs.lib.assertMsg (builtins.hasAttr "nixpkgs" inputs') (
+        nixpkgs.lib.traceSeqN 1 inputs' ''
+
+          In order to be able to use 'std.std.lib.fromMakesWith', an input
+          named 'nixpkgs' must be defined in the flake. See inputs above.
         ''
       );
       inputs';
     makes = nixpkgs.lib.fix (
       nixpkgs.lib.extends (
         _: _: {
-          inherit (system'.host) system;
+          inherit (inputsChecked.nixpkgs) system;
           inputs = inputsChecked;
           __nixpkgs__ = nixpkgs;
           __nixpkgsSrc__ = nixpkgs.path;
         }
       )
       (
-        import (inputsChecked.makes + /src/args/agnostic.nix) { inherit (system'.host) system; }
+        import (inputsChecked.makes + /src/args/agnostic.nix) { inherit (inputsChecked.nixpkgs) system; }
       )
       .__unfix__
     );
