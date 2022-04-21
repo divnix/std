1b03f3057b58dd7649378708d4eae78b7f337e16
David Arnold <david.arnold@iohk.io>
2022-04-20 23:46:38 -0500

    imp: add mdbook support & integration with ADRs

--- cells/std/devshellProfiles.nix
+++ cells/std/devshellProfiles.nix
@@ -22,10 +22,50 @@ in {
       commands = [
         {package = std;}
         {package = nixpkgs.adrgen;}
+        {package = nixpkgs.mdbook;}
       ];
-      devshell.startup.init-adrten = nixpkgs.lib.stringsWithDeps.noDepEntry ''
-        ${nixpkgs.adrgen}/bin/adrgen init "docs/adrs"
+      devshell.startup.init-adrgen = nixpkgs.lib.stringsWithDeps.noDepEntry ''
+        if [ ! -d "docs/architecture-decisions" ]; then
+          ${nixpkgs.adrgen}/bin/adrgen init "docs/architecture-decisions"
+        fi
       '';
+      devshell.startup.init-mdbook = let
+        config = {
+        };
+      in
+        nixpkgs.lib.stringsWithDeps.noDepEntry ''
+          if [ ! -f "book.toml" ]; then
+          mkdir -p docs
+          cat << EOF > book.toml
+          [book]
+          language = "en"
+          multilingual = false
+          src = "docs"
+          title = "Documentation"
+
+          [build]
+          build-dir = "docs/book"
+
+          EOF
+          cat << EOF > docs/SUMMARY.md
+          # Summary
+
+          EOF
+          fi
+          if [ ! -f "docs/.gitignore" ]; then
+          cat << EOF > docs/.gitignore
+          # mdbook build
+          book/**
+          EOF
+          fi
+          if [ ! $(grep "book/**" "docs/.gitignore") ]; then
+          cat << EOF >> docs/.gitignore
+
+          # mdbook build
+          book/**
+          EOF
+          fi
+        '';
     };
   };
 }
