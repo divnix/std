097136e80a28f273eae30245ee30eeeb995c0190
David Arnold <david.arnold@iohk.io>
2022-04-30 17:00:23 -0500

    imp: escape hatch do disable docs & adr nudging

--- cells/std/devshellProfiles.nix
+++ cells/std/devshellProfiles.nix
@@ -2,58 +2,62 @@
   inputs,
   cell,
 }: let
+  l = nixpkgs.lib // builtins;
   std = cell.cli.default;
   nixpkgs = inputs.nixpkgs;
 in {
-  default = {config, ...}: {
+  default = {config, ...}: let
+    cfg = config.std;
+  in {
+    options.std = {
+      adr.enable = (l.mkEnableOption "Enable ADR nudging") // {default = true;};
+      docs.enable = (l.mkEnableOption "Enable Docs nudging") // {default = true;};
+    };
     config = {
       commands = [
         {package = std;}
         {package = nixpkgs.adrgen;}
         {package = nixpkgs.mdbook;}
       ];
-      devshell.startup.init-adrgen = nixpkgs.lib.stringsWithDeps.noDepEntry ''
+      devshell.startup.init-adrgen = l.mkIf cfg.adr.enable (l.stringsWithDeps.noDepEntry ''
         if [ ! -d "docs/architecture-decisions" ]; then
           ${nixpkgs.adrgen}/bin/adrgen init "docs/architecture-decisions"
         fi
-      '';
-      devshell.startup.init-mdbook = let
-        config = {};
-      in
-        nixpkgs.lib.stringsWithDeps.noDepEntry ''
-          if [ ! -f "book.toml" ]; then
-          mkdir -p docs
-          cat << EOF > book.toml
-          [book]
-          language = "en"
-          multilingual = false
-          src = "docs"
-          title = "Documentation"
+      '');
+      devshell.startup.init-mdbook = l.mkIf cfg.docs.enable (l.stringsWithDeps.noDepEntry ''
+        if [ ! -f "book.toml" ]; then
+        mkdir -p docs
+        cat << EOF > book.toml
+        [book]
+        language = "en"
+        multilingual = false
+        src = "docs"
+        title = "Documentation"
 
-          [build]
-          build-dir = "docs/book"
+        [build]
+        build-dir = "docs/book"
 
-          EOF
-          cat << EOF > docs/SUMMARY.md
-          # Summary
+        EOF
+        cat << EOF > docs/SUMMARY.md
+        # Summary
 
-          EOF
-          fi
-          if [ ! -f "docs/.gitignore" ]; then
-          cat << EOF > docs/.gitignore
-          # mdbook build
-          book/**
-          EOF
-          fi
+        EOF
+        fi
+        if [ ! -f "docs/.gitignore" ]; then
+        cat << EOF > docs/.gitignore
+        # mdbook build
+        book/**
+        EOF
+        fi
 
-          if ! grep -qF 'book/' docs/.gitignore; then
-          cat << EOF >> docs/.gitignore
+        if ! grep -qF 'book/' docs/.gitignore; then
+        cat << EOF >> docs/.gitignore
 
-          # mdbook build
-          book/**
-          EOF
-          fi
-        '';
+        # mdbook build
+        book/**
+        EOF
+        fi
+      '');
     };
   };
 }
