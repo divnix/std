21715de4980407782749dfb7edfd6abe22e35ee9
David Arnold <david.arnold@iohk.io>
2022-05-30 19:34:30 -0500

    fix: oversight from 34e12eb21cf97b7b467b0f20ea7b27aacd7f85bc

--- src/clades/devshells.nix
+++ src/clades/devshells.nix
@@ -20,16 +20,18 @@
         description = "enter this devshell";
         command = ''
           std_layout_dir=$PRJ_ROOT/.std
-          std_layout_fragment_path=$std_layout_dir/${fragmentRelPath}
-          mkdir -p $std_layout_fragment_path
-          nix develop \
-            ${flake}#${fragment} \
-            --no-update-lock-file \
-            --no-write-lock-file \
-            --no-warn-dirty \
-            --accept-flake-config \
-            --profile "$std_layout_fragment_path/profile" \
-            --command "$SHELL"
+          profile_path="$std_layout_dir/${fragmentRelPath}"
+          mkdir -p "$profile_path"
+          nix_args=(
+            "${flake}#${fragment}"
+            "--no-update-lock-file"
+            "--no-write-lock-file"
+            "--no-warn-dirty"
+            "--accept-flake-config"
+            "--keep-outputs"
+          )
+          nix build "''${nix_args[@]}" --profile "$profile_path/shell-profile"
+          nix develop "''${nix_args[@]}" --profile "$profile_path/env-profile" --command "$SHELL"
         '';
       }
     ];
