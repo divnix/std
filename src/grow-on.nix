5a2c93b0c263e4b0f87f74164db1d9e7bd2ccf01
David Arnold <david.arnold@iohk.io>
2022-05-30 22:41:10 -0500

    eclint

--- src/grow-on.nix
+++ src/grow-on.nix
@@ -18,40 +18,40 @@
     See `std.harvest` for how to level-up outputs conveniently.
 
     Example use:
-   ```nix
-   {
-     inputs = {
-       # ...
-       std.url = "github:divnix/std";
-     };
+  ```nix
+  {
+    inputs = {
+      # ...
+      std.url = "github:divnix/std";
+    };
 
-     outputs = inputs: let
-       tasks = import ./nix/std.nix inputs;
-       lib = import ./nix/lib.nix inputs;
-     in
-       inputs.std.growOn {
-         inherit inputs;
-         cellsFrom = ./cells;
-         organelles = [
-           # ...
-         ];
-       }
-       # Soil ...
-       # nix-cli compat
-       {
-         devShell = inputs.std.harvest inputs.self ["tullia" "devshell" "default"];
-         defaultPackage = inputs.std.harvest inputs.self ["tullia" "apps" "tullia"];
-       }
-       # dog food
-       (lib.fromStd {
-         actions = inputs.std.harvest inputs.self ["tullia" "action"];
-         tasks = inputs.std.harvest inputs.self ["tullia" "task"];
-       })
-       # top level tullia outputs
-       (lib // {inherit tasks;});
-   }
-   ```
-   */
+    outputs = inputs: let
+      tasks = import ./nix/std.nix inputs;
+      lib = import ./nix/lib.nix inputs;
+    in
+      inputs.std.growOn {
+        inherit inputs;
+        cellsFrom = ./cells;
+        organelles = [
+          # ...
+        ];
+      }
+      # Soil ...
+      # nix-cli compat
+      {
+        devShell = inputs.std.harvest inputs.self ["tullia" "devshell" "default"];
+        defaultPackage = inputs.std.harvest inputs.self ["tullia" "apps" "tullia"];
+      }
+      # dog food
+      (lib.fromStd {
+        actions = inputs.std.harvest inputs.self ["tullia" "action"];
+        tasks = inputs.std.harvest inputs.self ["tullia" "task"];
+      })
+      # top level tullia outputs
+      (lib // {inherit tasks;});
+  }
+  ```
+  */
   growOn = args:
     grow args
     // {
--- src/harvest.nix
+++ src/harvest.nix
@@ -1,23 +1,23 @@
 {nixpkgs}: let
   l = nixpkgs.lib // builtins;
   /*
-   A function that "up-lifts" your `std` targets.
+  A function that "up-lifts" your `std` targets.
 
-   The transformation is: system.cell.organelle.target -> system.target
+  The transformation is: system.cell.organelle.target -> system.target
 
-   You can typically use this function in a compatibility layer of soil.
+  You can typically use this function in a compatibility layer of soil.
 
-   Example:
+  Example:
 
-   ```nix
-   # Soil ...
-   # nix-cli compat
-   {
-     devShell = inputs.std.harvest inputs.self ["tullia" "devshell" "default"];
-     defaultPackage = inputs.std.harvest inputs.self ["tullia" "apps" "tullia"];
-   }
-   ```
-   */
+  ```nix
+  # Soil ...
+  # nix-cli compat
+  {
+    devShell = inputs.std.harvest inputs.self ["tullia" "devshell" "default"];
+    defaultPackage = inputs.std.harvest inputs.self ["tullia" "apps" "tullia"];
+  }
+  ```
+  */
   harvest = t: p:
     l.mapAttrs (_: v: l.getAttrFromPath p v)
     (
