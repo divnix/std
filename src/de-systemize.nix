72e384ce8e22f04b63d3c5d9b02ba4a6704f96c5
David Arnold <david.arnold@iohk.io>
2022-07-04 22:22:36 -0500

    fix: deSystemize hoisting a function
    - in case we're hoising a function upon the system level, we need to
      make it a functor, instead.
    - take the opportunity to make that function recursive (with a cut-off
      to remain speedy)

--- src/de-systemize.nix
+++ src/de-systemize.nix
@@ -7,27 +7,16 @@ let
 
   You can use this function independently of the rest of std.
   */
-  deSystemize = system: fragment:
-    if l.isAttrs fragment && l.hasAttr "${system}" fragment
-    then fragment // fragment.${system}
-    else
-      l.mapAttrs (
-        # _ consumes input's name
-        # fragment -> maybe systems
-        _: fragment:
-          if l.isAttrs fragment && l.hasAttr "${system}" fragment
-          then fragment // fragment.${system}
-          else
-            l.mapAttrs (
-              # _ consumes input's output's name
-              # fragment -> maybe systems
-              _: fragment:
-                if l.isAttrs fragment && l.hasAttr "${system}" fragment
-                then (fragment // fragment.${system})
-                else fragment
-            )
-            fragment
-      )
-      fragment;
+  deSystemize = let
+    iteration = cutoff: system: fragment:
+      if ! (l.isAttrs fragment) || cutoff == 0
+      then fragment
+      else if l.hasAttr "${system}" fragment && ! l.isFunction fragment.${system}
+      then fragment // fragment.${system}
+      else if l.hasAttr "${system}" fragment && l.isFunction fragment.${system}
+      then fragment // {__functor = _: fragment.${system};}
+      else l.mapAttrs (_: iteration (cutoff - 1) system) fragment;
+  in
+    iteration 3;
 in
   deSystemize
