7f138eaa0cdfb7f8fae628ced99b7c29eba241de
David Arnold <david.arnold@iohk.io>
2022-07-25 23:30:20 -0500

    feat(std): integrate devshell & nixago tightly

--- cells/std/lib/default.nix
+++ cells/std/lib/default.nix
@@ -4,19 +4,74 @@
 }: let
   nixpkgs = inputs.nixpkgs;
 in {
-  inherit (inputs.devshell.legacyPackages) mkShell;
-  mkNixago = configuration:
-    (inputs.nixago.lib.make configuration)
-    // {
-      # transparently extend config data with a functor
-      __functor = _: extra: (inputs.nixago.lib.make (configuration
-        // {
-          configData = inputs.data-merge.merge configuration.configData extra;
-        }));
-      # implement a minimal numtide/devshell forward contract
-      packages = configuration.packages or [];
-      commands = configuration.commands or [];
+  mkShell = configuration: let
+    nixagoModule = {
+      config,
+      lib,
+      ...
+    }:
+      with lib; let
+        cfg = config;
+      in {
+        options.nixago = mkOption {
+          type = types.listOf types.attrs;
+          default = [];
+          apply = x: builtins.catAttrs "__passthru" x;
+          description = "List of `std` Nixago pebbles to load";
+        };
+
+        config = mkIf (cfg.nixago != []) {
+          devshell = let
+            acc = nixpkgs.lib.foldl inputs.nixpkgs.lib.recursiveUpdate {};
+          in
+            acc (
+              []
+              ++ (builtins.map (o: o.devshell) cfg.nixago)
+              ++ [{startup.nixago-setup-hook = nixpkgs.lib.stringsWithDeps.noDepEntry (inputs.nixago.lib.makeAll cfg.nixago).shellHook;}]
+            );
+          packages = builtins.concatMap (o: o.packages) cfg.nixago;
+          commands = builtins.concatMap (o: o.commands) cfg.nixago;
+        };
+      };
+  in
+    inputs.devshell.legacyPackages.mkShell {
+      imports = [configuration nixagoModule];
     };
+
+  mkNixago = configuration: let
+    # implement a minimal numtide/devshell forward contract
+    configuration' =
+      configuration
+      // {
+        packages = configuration.packages or [];
+        commands = configuration.commands or [];
+        devshell = configuration.devshell or {};
+      };
+    # transparently extend config data with a functor
+    __functor = self: {
+      configData ? {},
+      packages ? [],
+      commands ? [],
+      devshell ? {},
+    }: let
+      __passthru = self.__passthru or configuration';
+      newSelf =
+        __passthru
+        // {
+          configData = inputs.data-merge.merge __passthru.configData configData;
+          packages = __passthru.packages ++ packages;
+          commands = __passthru.commands ++ commands;
+          devshell = inputs.nixpkgs.lib.recursiveUpdate __passthru.devshell devshell;
+        };
+    in
+      (inputs.nixago.lib.make newSelf)
+      // {
+        # keep here, cause nixago.lib.make would strip them
+        inherit __functor;
+        __passthru = newSelf;
+      };
+  in
+    __functor configuration' {};
 
   fromMakesWith = inputs': let
     inputsChecked = assert nixpkgs.lib.assertMsg (builtins.hasAttr "makes" inputs') (
--- cells/std/lib/mkShell.md
+++ cells/std/lib/mkShell.md
@@ -2,4 +2,26 @@
 
 This is a transparent convenience proxy for [`numtide/devshell`'s][numtide-devshell] `mkShell` function.
 
+It is enriched with a tight integration for `std` [Nixago][nixago] pebbles:
+
+```nix
+{ inputs, cell}: {
+  default = inputs.std.std.lib.mkShell {
+    /* ... */
+    nixago = [
+      (cell.nixago.foo {
+        configData.qux = "xyz";
+        packages = [ pkgs.additional-package ];
+      })
+      cell.nixago.bar
+      cell.nixago.quz
+    ];
+  };
+}
+```
+
+_Note, that you can extend any Nixago Pebble at the calling site
+via a built-in functor like in the example above._
+
+[nixago]: https://github.com/nix-community/nixago
 [numtide-devshell]: https://github.com/numtide/devshell
