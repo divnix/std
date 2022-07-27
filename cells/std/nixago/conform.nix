d8ebb6eda3e5646ebd9a04f11c934d249b7f8a3c
David Arnold <david.arnold@iohk.io>
2022-07-27 17:15:16 -0500

    feat(std): integrate some initial nixago standard pebbles

---
+++ cells/std/nixago/conform.nix
@@ -0,0 +1,20 @@
+{
+  commit = {
+    header = {length = 89;};
+    conventional = {
+      types = [
+        "build"
+        "chore"
+        "ci"
+        "docs"
+        "feat"
+        "fix"
+        "perf"
+        "refactor"
+        "style"
+        "test"
+      ];
+      scopes = [];
+    };
+  };
+}
---
+++ cells/std/nixago/mdbook.nix
@@ -0,0 +1,16 @@
+{
+  book = {
+    language = "en";
+    multilingual = false;
+    src = "docs";
+    title = "Documentation";
+  };
+  build = {
+    build-dir = "docs/book";
+  };
+  preprocessor = {
+    kroki-preprocessor = {
+      command = "mdbook-kroki-preprocessor";
+    };
+  };
+}
