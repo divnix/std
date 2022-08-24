8478eadc07bfa7da67fb94f2f6b43132f1a5d127
Yuriy Taraday <yuriy.taraday@tweag.io>
2022-08-24 17:31:10 +0400

    adrgen: Build from master
    While there's no release of adrgen with https://github.com/asiermarques/adrgen/pull/14
    build it from current master state that has only some doc changes in
    addition to that build fix.
    
    Related to #99.

--- cells/std/packages.nix
+++ cells/std/packages.nix
@@ -2,7 +2,7 @@
   inputs,
   cell,
 }: {
-  adrgen = inputs.nixpkgs.adrgen;
+  adrgen = inputs.nixpkgs.callPackage ./packages/adrgen.nix {};
   mdbook = inputs.nixpkgs.mdbook;
   mdbook-kroki-preprocessor = import ./packages/mdbook-kroki-preprocessor.nix {inherit inputs cell;};
 }
