a0b298c397a28615891c33536fcb6c9ffcc36a52
Joshua Gilman <joshuagilman@gmail.com>
2022-09-06 09:48:49 -0700

    fix: makes task description optional and changes steps to content

--- cells/automation/tasks.nix
+++ cells/automation/tasks.nix
@@ -1,8 +1,8 @@
 {
   fmt = {
     description = "Formats all changed source files";
-    steps = [
-      "treefmt $(git diff --name-only --cached)"
-    ];
+    content = ''
+      treefmt $(git diff --name-only --cached)
+    '';
   };
 }
