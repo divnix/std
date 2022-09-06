e1730b7b9888dc4404548179f8f48a0061a01c39
Joshua Gilman <joshuagilman@gmail.com>
2022-09-06 11:16:53 -0700

    feat: adds interpreter attribute to just tasks

--- cells/std/nixago.nix
+++ cells/std/nixago.nix
@@ -59,7 +59,22 @@ in
     };
     just = {
       configData = {};
-      apply = d: {data = d;}; # CUE expects structure to be wrapped with "data"
+      apply = d: let
+        # Transforms interpreter attribute if present
+        # nixpkgs.pkgname -> nixpkgs.pkgname + '/bin/<name>'
+        getExe = x: "${l.getBin x}/bin/${x.meta.mainProgram or (l.getName x)}";
+        final =
+          d
+          // {
+            tasks =
+              l.mapAttrs
+              (n: v:
+                v // l.optionalAttrs (v ? interpreter) {interpreter = getExe v.interpreter;})
+              d.tasks;
+          };
+      in {
+        data = final; # CUE expects structure to be wrapped with "data"
+      };
       format = "text";
       output = "Justfile";
       packages = [nixpkgs.just];
