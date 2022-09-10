69a02240fea378018dfd7ed03f18cc6f1af014ea
Joshua Gilman <joshuagilman@gmail.com>
2022-09-10 09:40:27 -0700

    refactor: splits mdbook to default/preset parts

--- cells/presets/nixago/mdbook.nix
+++ cells/presets/nixago/mdbook.nix
@@ -1,13 +1,4 @@
 {
-  book = {
-    language = "en";
-    multilingual = false;
-    src = "docs";
-    title = "Documentation";
-  };
-  build = {
-    build-dir = "docs/book";
-  };
   preprocessor = {
     kroki-preprocessor = {
       command = "mdbook-kroki-preprocessor";
---
+++ cells/std/nixago/mdbook.nix
@@ -0,0 +1,11 @@
+{
+  book = {
+    language = "en";
+    multilingual = false;
+    src = "docs";
+    title = "Documentation";
+  };
+  build = {
+    build-dir = "docs/book";
+  };
+}
