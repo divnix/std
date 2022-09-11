bff0e737b872af59ba70fdf6e06b7dc212156d17
David Arnold <david.arnold@iohk.io>
2022-09-10 21:16:41 -0500

    fix: declutter mdbook contracts between preset and nixago pebble

--- cells/presets/nixago/mdbook.nix
+++ cells/presets/nixago/mdbook.nix
@@ -1,4 +1,13 @@
 {
+  book = {
+    language = "en";
+    multilingual = false;
+    src = "docs";
+    title = "Documentation";
+  };
+  build = {
+    build-dir = "docs/book";
+  };
   preprocessor = {
     kroki-preprocessor = {
       command = "mdbook-kroki-preprocessor";
