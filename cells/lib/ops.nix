d165f5f59b698051a4cfa28a2f4b7c72eed14686
David Arnold <david.arnold@iohk.io>
2022-10-04 16:23:30 -0500

    fix(lib): use of requireInput

--- cells/lib/ops.nix
+++ cells/lib/ops.nix
@@ -5,10 +5,10 @@
   inherit (inputs.cells.std.errors) requireInput;
 in {
   mkMicrovm = import ./ops/mkMicrovm.nix {
-    inputs = requireInput "microvm" "github:astro/microvm.nix" "std.lib.ops.mkMicrovm" inputs;
+    inputs = requireInput "microvm" "github:astro/microvm.nix" "std.lib.ops.mkMicrovm";
   };
 
   writeShellEntrypoint = import ./ops/writeShellEntrypoint.nix {
-    inputs = requireInput "n2c" "github:nlewo/nix2container" "std.lib.ops.writeShellEntrypoint" inputs;
+    inputs = requireInput "n2c" "github:nlewo/nix2container" "std.lib.ops.writeShellEntrypoint";
   };
 }
