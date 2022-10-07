c888e15df29be54f51fe5dc547b34a8424cbf265
Joshua Gilman <joshuagilman@gmail.com>
2022-10-07 14:06:00 -0700

    feat: makes vscode dependencies optional

--- cells/_automation/containers.nix
+++ cells/_automation/containers.nix
@@ -16,7 +16,22 @@ in
       url = "https://github.com/divnix";
       source = "https://github.com/divnix";
       description = ''
-        A prepackaged devcontainer for hacking on std
+        A prepackaged container for hacking on std
+      '';
+    };
+  };
+  vscode = lib.ops.mkDevOCI {
+    name = "docker.io/std-vscode";
+    tag = "latest";
+    devshell = inputs.cells._automation.devshells.default;
+    vscode = true;
+    labels = {
+      title = "std-dev";
+      version = "0.1.0";
+      url = "https://github.com/divnix";
+      source = "https://github.com/divnix";
+      description = ''
+        A prepackaged vscode devcontainer for hacking on std
       '';
     };
   };
--- cells/lib/ops/mkDevOCI.nix
+++ cells/lib/ops/mkDevOCI.nix
@@ -1,52 +1,51 @@
-{
-  inputs,
-  cell,
-}: let
+{ inputs
+, cell
+,
+}:
+let
   inherit (inputs) nixpkgs std;
   l = nixpkgs.lib // builtins;
   n2c = inputs.n2c.packages.nix2container;
 in
-  {
-    name,
-    devshell,
-    runtimeShell ? nixpkgs.bashInteractive,
-    user ? "vscode",
-    tag ? "",
-    setup ? [],
-    perms ? [],
-    labels ? {},
-    options ? {},
-  }: let
-    # Apply the correct hook based on the given runtime shell
-    # Only bash/zsh are supported currently
-    shellName = builtins.unsafeDiscardStringContext (l.baseNameOf (l.getExe runtimeShell));
-    shellConfigs = {
-      bash = "bashrc";
-      zsh = "zshrc";
-    };
+{ name
+, devshell
+, runtimeShell ? nixpkgs.bashInteractive
+, user ? "user"
+, vscode ? false
+, tag ? ""
+, setup ? [ ]
+, perms ? [ ]
+, labels ? { }
+, options ? { }
+,
+}:
+let
+  # vscode defaults to "vscode" as the user
+  user' = if vscode then "vscode" else user;
 
-    # Configure local user
-    setupUser = cell.ops.mkUser {
-      inherit user;
-      group = user;
-      uid = "1000";
-      gid = "1000";
-      shell = l.getExe runtimeShell;
-      withHome = true;
-      withRoot = true;
-    };
+  # Apply the correct hook based on the given runtime shell
+  # Only bash/zsh are supported currently
+  shellName = builtins.unsafeDiscardStringContext (l.baseNameOf (l.getExe runtimeShell));
+  shellConfigs = {
+    bash = "bashrc";
+    zsh = "zshrc";
+  };
 
-    # Configure direnv, git, and nix. Additionally, perform some setup for
-    # vscode which makes some basic assumptions about the environment.
-    setupEnv =
-      cell.ops.mkSetup "container"
+  # Configure local user
+  setupUser = cell.ops.mkUser {
+    user = user';
+    group = user';
+    uid = "1000";
+    gid = "1000";
+    shell = l.getExe runtimeShell;
+    withHome = true;
+    withRoot = true;
+  };
+
+  # Configure direnv, git, and nix
+  setupEnv =
+    cell.ops.mkSetup "container"
       [
-        {
-          regex = "/vscode";
-          mode = "0744";
-          uid = 1000;
-          gid = 1000;
-        }
         {
           regex = "/tmp";
           mode = "0777";
@@ -55,9 +54,6 @@ in
       ''
         # Setup tmp folder
         mkdir -p $out/tmp
-
-        # Setup vscode directory
-        mkdir -p $out/vscode
 
         # Enable nix flakes
         mkdir -p $out/etc
@@ -85,6 +81,23 @@ in
           [safe]
               directory = *
         EOF
+      '';
+
+  # Setup the environment in such a way to make it compatible with what a
+  # vscode devcontainer expects
+  setupVSCode =
+    cell.ops.mkSetup "vscode"
+      [
+        {
+          regex = "/vscode";
+          mode = "0744";
+          uid = 1000;
+          gid = 1000;
+        }
+      ]
+      ''
+        # Setup vscode directory
+        mkdir -p $out/vscode
 
         # vscode uses /bin/sh for running commands
         mkdir -p $out/bin
@@ -98,117 +111,113 @@ in
         ln -s ${nixpkgs.coreutils}/bin/env $out/usr/bin/env
       '';
 
-    # These packages are required by nix and its direnv integration test
-    nixDeps = [
-      nixpkgs.direnv
-      nixpkgs.git
-      nixpkgs.nix
-      nixpkgs.gawk
-      nixpkgs.gnugrep
-      nixpkgs.gnused
-      nixpkgs.diffutils
-    ];
+  # These packages are required by nix and its direnv integration test
+  nixDeps = [
+    nixpkgs.direnv
+    nixpkgs.git
+    nixpkgs.nix
+    nixpkgs.gawk
+    nixpkgs.gnugrep
+    nixpkgs.gnused
+    nixpkgs.diffutils
+  ];
 
-    # These packages are required by vscode
-    vscodeDeps = [
-      nixpkgs.gnutar
-      nixpkgs.gzip
-    ];
+  # These are common packages that are useful for development
+  commonDeps = [
+    nixpkgs.nano
+    nixpkgs.gnupg
+    nixpkgs.openssh
+  ];
 
-    # These are common packages that are useful for development
-    commonDeps = [
-      nixpkgs.nano
-      nixpkgs.gnupg
-      nixpkgs.openssh
-    ];
+  # These packages are required by vscode
+  vscodeDeps = [
+    nixpkgs.gnutar
+    nixpkgs.gzip
+  ];
 
-    # The entrypoint should be long-running by default
-    entrypoint = cell.ops.writeScript {
-      name = "entrypoint";
-      text = ''
-        #!${l.getExe runtimeShell}
+  # The entrypoint should be long-running by default
+  entrypoint = cell.ops.writeScript {
+    name = "entrypoint";
+    text = ''
+      #!${l.getExe runtimeShell}
 
-        if [ $# -eq 0 ]; then
-            while :; do sleep 2073600; done
-        else
-            "$@" &
-        fi
+      if [ $# -eq 0 ]; then
+          while :; do sleep 2073600; done
+      else
+          "$@" &
+      fi
 
-        wait -n
-      '';
-    };
-  in
-    cell.ops.mkOCI {
-      inherit entrypoint name tag labels perms;
+      wait -n
+    '';
+  };
+in
+cell.ops.mkOCI {
+  inherit entrypoint name tag labels perms;
 
-      # No particular reason for using 1000 here other than it's idiomatic
-      uid = "1000";
-      gid = "1000";
+  # No particular reason for using 1000 here other than it's idiomatic
+  uid = "1000";
+  gid = "1000";
 
-      setup =
-        [
-          setupEnv
-          setupUser
-        ]
-        ++ setup;
+  setup = [ setupEnv setupUser ]
+    ++ setup
+    ++ (l.optionals vscode [ setupVSCode ]);
 
-      layers = [
-        (n2c.buildLayer {
-          copyToRoot = [
-            (nixpkgs.buildEnv
-              {
-                name = "devshell";
-                paths =
-                  [
-                    nixpkgs.coreutils
-                    devshell
-                    runtimeShell
-                  ]
-                  ++ commonDeps
-                  ++ nixDeps
-                  ++ vscodeDeps;
+  layers = [
+    (n2c.buildLayer {
+      copyToRoot = [
+        (nixpkgs.buildEnv
+          {
+            name = "devshell";
+            paths =
+              [
+                nixpkgs.coreutils
+                devshell
+                runtimeShell
+              ]
+              ++ commonDeps
+              ++ nixDeps
+              ++ (l.optionals vscode vscodeDeps);
 
-                pathsToLink = ["/bin"];
-              })
-            # Required for fetching additional packages
-            nixpkgs.cacert
-          ];
-          maxLayers = 100;
-        })
+            pathsToLink = [ "/bin" ];
+          })
+        # Required for fetching additional packages
+        nixpkgs.cacert
       ];
+      maxLayers = 100;
+    })
+  ];
 
-      options = l.recursiveUpdate options {
-        # Initialize the nix database so we can use the nix CLI
-        initializeNixDatabase = true;
+  options = l.recursiveUpdate options {
+    # Initialize the nix database so we can use the nix CLI
+    initializeNixDatabase = true;
 
-        # This configures a single-user environment where the container user
-        # owns all of /nix
-        nixUid = 1000;
-        nixGid = 1000;
+    # This configures a single-user environment where the container user
+    # owns all of /nix
+    nixUid = 1000;
+    nixGid = 1000;
 
-        config = {
-          Env = [
-            # Tell direnv to find it's config in /etc
-            "DIRENV_CONFIG=/etc"
-            # Required by many tools
-            "HOME=/home/${user}"
-            # Nix related environment variables
-            "NIX_CONF_DIR=/etc"
-            "NIX_PAGER=cat"
-            # This file is created when nixpkgs.cacert is copied to the root
-            "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
-            # Pin <nixpkgs> to the version used to build the container
-            "NIX_PATH=nixpkgs=${nixpkgs.path}"
-            # Nix expects a user to be set
-            "USER=${user}"
-            # vscode ships with its own nodejs binary that it uploads when the
-            # container is started. It is, unfortunately, dynamically linked and
-            # we need to resort to some hackery to get it to run.
-            "LD_LIBRARY_PATH=${nixpkgs.stdenv.cc.cc.lib}/lib"
-          ];
-          Volumes = {
-            "/vscode" = {};
-          };
-        };
-      };
-    }
+    config = {
+      Env = [
+        # Tell direnv to find it's config in /etc
+        "DIRENV_CONFIG=/etc"
+        # Required by many tools
+        "HOME=/home/${user'}"
+        # Nix related environment variables
+        "NIX_CONF_DIR=/etc"
+        "NIX_PAGER=cat"
+        # This file is created when nixpkgs.cacert is copied to the root
+        "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
+        # Pin <nixpkgs> to the version used to build the container
+        "NIX_PATH=nixpkgs=${nixpkgs.path}"
+        # Nix expects a user to be set
+        "USER=${user'}"
+      ] ++ (l.optionals vscode [
+        # vscode ships with its own nodejs binary that it uploads when the
+        # container is started. It is, unfortunately, dynamically linked and
+        # we need to resort to some hackery to get it to run.
+        "LD_LIBRARY_PATH=${nixpkgs.stdenv.cc.cc.lib}/lib"
+      ]);
+      Volumes = (l.optionalAttrs vscode { "/vscode" = { }; });
+    };
+  };
+}
