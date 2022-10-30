758251046fddee9464e58d423a4c7975d7d3411d
David Arnold <david.arnold@iohk.io>
2022-10-29 19:38:22 -0500

    feat: remove arion's nixos module in std
    in the context of the std integration paradigm, it's a layer violation

--- cells/lib/dev/mkArion.md
+++ cells/lib/dev/mkArion.md
@@ -2,4 +2,14 @@
 
 This is a transparent convenience proxy for [`hercules-ci/arion`'s][arion] `lib.build` function.
 
+However, the arion's `nixos` config option was removed.
+
+As Standard claims to be the integration layer it will not delegate integration via a foreign
+interface to commissioned tools, such as arion.
+
+This is a bridge towards and from docker-compose users. Making nixos part of the interface would
+likely alienate that bridge for those users.
+
+If you need a nixos-based container image, please check out the arion source code on how it's done.
+
 [arion]: https://github.com/hercules-ci/arion
--- cells/lib/dev/mkArion.nix
+++ cells/lib/dev/mkArion.nix
@@ -3,9 +3,40 @@
   l,
 }: let
   inherit (inputs) arion nixpkgs;
+
+  disabledNotice = ''
+    divnix/std disabled arion's nixos instrumentation.
+
+    Standard being the horizontal integration layer it would be a layer violation
+    to delegate integration to a commissioned tool.
+
+    Doing this would reduce the mental clarity of std since a foreign integration
+    pattern would have to be supported.
+
+    If you want to create a container that uses NixOS + systemd as its init-system,
+    please find out how it's done here:
+      ${arion}/src/nix/service/nixos-init.nix
+
+    You can then use the normal container block type to create your image and
+    pass it to your arion configuration.
+  '';
+
+  disableNixosModule = {
+    disabledModules = [
+      (arion + /src/nix/nixos/container-systemd.nix)
+      (arion + /src/nix/nixos/default-shell.nix)
+      (arion + /src/nix/service/nixos.nix)
+      (arion + /src/nix/service/nixos-init.nix)
+    ];
+    imports = [
+      (l.mkRemovedOptionModule ["nixos" "configuration"] disabledNotice)
+      (l.mkRemovedOptionModule ["nixos" "build"] disabledNotice)
+      (l.mkRemovedOptionModule ["nixos" "evaluatedConfig"] disabledNotice)
+    ];
+  };
 in
   module:
     arion.lib.eval {
-      modules = [module];
+      modules = [disableNixosModule module];
       pkgs = nixpkgs;
     }
