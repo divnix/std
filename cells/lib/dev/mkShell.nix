28deea8766bc3749d51b7c6864a36edcc7447571
David Arnold <david.arnold@iohk.io>
2022-11-12 13:09:40 -0500

    fix: flip-flop side-effect where treefmt battles nixago autogeneration
    closes: #182

--- cells/lib/dev/mkShell.nix
+++ cells/lib/dev/mkShell.nix
@@ -22,18 +22,38 @@ in
           description = "List of `std` Nixago pebbles to load";
         };
 
-        config = mkIf (cfg.nixago != []) {
-          devshell = let
-            acc = l.foldl l.recursiveUpdate {};
-          in
-            acc (
-              []
-              ++ (l.map (o: o.devshell) cfg.nixago)
-              ++ [{startup.nixago-setup-hook = l.stringsWithDeps.noDepEntry (nixago.lib.makeAll cfg.nixago).shellHook;}]
-            );
-          packages = l.concatMap (o: o.packages) cfg.nixago;
-          commands = l.concatMap (o: o.commands) cfg.nixago;
-        };
+        config = let
+          # effectuate side effects on treefmt nixago, if present
+          # to prevent treefmt from formatting auto-generated files
+          partitioned = l.partition (n: n.output == "treefmt.toml") cfg.nixago;
+          treefmt' =
+            l.map (
+              t:
+                l.recursiveUpdate t
+                {
+                  configData.global.excludes =
+                    t.configData.global.excludes
+                    or []
+                    ++ (l.map (o: o.output) cfg.nixago);
+                }
+            )
+            # if there's more than one treefmt, that's a malconfiguration
+            # but here: we don't deal with that case
+            partitioned.right;
+          updated = treefmt' ++ partitioned.wrong;
+        in
+          mkIf (cfg.nixago != []) {
+            devshell = let
+              acc = l.foldl l.recursiveUpdate {};
+            in
+              acc (
+                []
+                ++ (l.map (o: o.devshell) updated)
+                ++ [{startup.nixago-setup-hook = l.stringsWithDeps.noDepEntry (nixago.lib.makeAll updated).shellHook;}]
+              );
+            packages = l.concatMap (o: o.packages) updated;
+            commands = l.concatMap (o: o.commands) updated;
+          };
       };
   in
     devshell.legacyPackages.mkShell {
