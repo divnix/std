c06c81f22e1accead9bd6194f73de6ea2e9ce03c
David Arnold <david.arnold@iohk.io>
2022-11-12 13:02:03 -0500

    style: treefmt

--- src/blocktypes/actions/build.nix
+++ src/blocktypes/actions/build.nix
@@ -1,8 +1,7 @@
-flake: fragment:
-      {
-        name = "build";
-        description = "build this target";
-        command = ''
-          nix build ${flake}#${fragment}
-        '';
-      }
+flake: fragment: {
+  name = "build";
+  description = "build this target";
+  command = ''
+    nix build ${flake}#${fragment}
+  '';
+}
