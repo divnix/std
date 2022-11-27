18fc2ed09072baf480801fc99b10429f81c181f3
David Arnold <david.arnold@iohk.io>
2022-11-27 15:44:32 -0500

    fix(std): nixago editorconfig special ini format
    fix: #188

--- cells/std/nixago/editorconfig.nix
+++ cells/std/nixago/editorconfig.nix
@@ -2,10 +2,19 @@
   inputs,
   cell,
 }: let
+  l = nixpkgs.lib // builtins;
   inherit (inputs) nixpkgs;
 in {
   configData = {};
   output = ".editorconfig";
-  format = "ini";
+  engine = request: let
+    inherit (request) configData output;
+    name = l.baseNameOf output;
+    value = {
+      globalSection = {root = configData.root or true;};
+      sections = l.removeAttrs configData ["root"];
+    };
+  in
+    nixpkgs.writeText name (l.generators.toINIWithGlobalSection {} value);
   packages = [nixpkgs.editorconfig-checker];
 }
