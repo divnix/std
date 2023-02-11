41c08d12c9b6a07150960b4901e6ec365bbd2772
David Arnold <david.arnold@iohk.io>
2023-02-11 11:32:39 -0500

    style: treefmt

--- cells/presets/nixago.nix
+++ cells/presets/nixago.nix
@@ -5,39 +5,40 @@
   inherit (inputs) nixpkgs;
   inherit (inputs.cells) std;
   l = nixpkgs.lib // builtins;
-   inherit (import (inputs.self + /deprecation.nix) inputs) warnNixagoOutfactored;
-in l.mapAttrs (_: warnNixagoOutfactored) {
-  adrgen = std.nixago.adrgen {
-    data = import ./nixago/adrgen.nix;
-  };
-  editorconfig = std.nixago.editorconfig {
-    data = import ./nixago/editorconfig.nix;
-    hook.mode = "copy"; # already useful before entering the devshell
-  };
-  conform = std.nixago.conform {
-    data = import ./nixago/conform.nix;
-  };
-  lefthook = std.nixago.lefthook {
-    data = import ./nixago/lefthook.nix;
-  };
-  mdbook = std.nixago.mdbook {
-    data = import ./nixago/mdbook.nix;
-    hook.mode = "copy"; # let CI pick it up outside of devshell
-    packages = [std.packages.mdbook-kroki-preprocessor];
-  };
-  treefmt = std.nixago.treefmt {
-    data = import ./nixago/treefmt.nix;
-    packages = [
-      nixpkgs.alejandra
-      nixpkgs.nodePackages.prettier
-      nixpkgs.nodePackages.prettier-plugin-toml
-      nixpkgs.shfmt
-    ];
-    devshell.startup.prettier-plugin-toml = l.stringsWithDeps.noDepEntry ''
-      export NODE_PATH=${nixpkgs.nodePackages.prettier-plugin-toml}/lib/node_modules:$NODE_PATH
-    '';
-  };
-  githubsettings = std.nixago.githubsettings {
-    data = import ./nixago/githubsettings.nix;
-  };
-}
+  inherit (import (inputs.self + /deprecation.nix) inputs) warnNixagoOutfactored;
+in
+  l.mapAttrs (_: warnNixagoOutfactored) {
+    adrgen = std.nixago.adrgen {
+      data = import ./nixago/adrgen.nix;
+    };
+    editorconfig = std.nixago.editorconfig {
+      data = import ./nixago/editorconfig.nix;
+      hook.mode = "copy"; # already useful before entering the devshell
+    };
+    conform = std.nixago.conform {
+      data = import ./nixago/conform.nix;
+    };
+    lefthook = std.nixago.lefthook {
+      data = import ./nixago/lefthook.nix;
+    };
+    mdbook = std.nixago.mdbook {
+      data = import ./nixago/mdbook.nix;
+      hook.mode = "copy"; # let CI pick it up outside of devshell
+      packages = [std.packages.mdbook-kroki-preprocessor];
+    };
+    treefmt = std.nixago.treefmt {
+      data = import ./nixago/treefmt.nix;
+      packages = [
+        nixpkgs.alejandra
+        nixpkgs.nodePackages.prettier
+        nixpkgs.nodePackages.prettier-plugin-toml
+        nixpkgs.shfmt
+      ];
+      devshell.startup.prettier-plugin-toml = l.stringsWithDeps.noDepEntry ''
+        export NODE_PATH=${nixpkgs.nodePackages.prettier-plugin-toml}/lib/node_modules:$NODE_PATH
+      '';
+    };
+    githubsettings = std.nixago.githubsettings {
+      data = import ./nixago/githubsettings.nix;
+    };
+  }
