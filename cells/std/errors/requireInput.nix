4db9fcc3ee1fbed3487bf0f1184caac2fb244882
David Arnold <david.arnold@iohk.io>
2023-02-12 14:18:16 -0500

    feat: optimize standard warnings
    closes: #258

--- cells/std/errors/requireInput.nix
+++ cells/std/errors/requireInput.nix
@@ -2,33 +2,68 @@
   l = inputs.nixpkgs.lib // builtins;
 
   # other than `divnix/blank`
-  condition = inputs.${input}.sourceInfo.narHash != "sha256-O8/MWsPBGhhyPoPLHZAuoZiiHo9q6FLlEeIDEXuj6T4=";
+  isBlank = input: inputs.${input}.sourceInfo.narHash == "sha256-O8/MWsPBGhhyPoPLHZAuoZiiHo9q6FLlEeIDEXuj6T4=";
 
-  trace = l.traceSeqN 1 inputs;
-in
-  assert l.assertMsg condition (trace ''
+  ansi = import ./ansi.nix;
 
-    ===============================================
-    !!!  🚜️   STANDARD INPUT OVERLOADING   🚜️   !!!
-    -----------------------------------------------
-    In order to be able to use this target, an
-    extra input must be overloaded onto Standard
-    -----------------------------------------------
-    Target:      ${target}
-    Extra Input: ${input}
-    Url:         ${url}
-    -----------------------------------------------
-    To fix this, add this to your './flake.nix':
+  pad = n: s: let
+    prefix = l.concatStringsSep "" (l.genList (_: " ") n);
+  in
+    prefix + s;
+
+  indent = s: let
+    n = 5;
+    prefix = l.concatStringsSep "" (l.genList (_: " ") n);
+    lines = l.splitString "\n" s;
+  in
+    l.concatStringsSep "\n${prefix}│ " lines;
+
+  warn = let
+    apply =
+      l.replaceStrings
+      (map (key: "{${key}}") (l.attrNames ansi))
+      (l.attrValues ansi);
+  in
+    msg: l.trace (apply "🚀 {bold}{200}Standard Input Overloading{reset}${msg}") "";
+
+  body = ''
+    In order to use ${target}, add to {bold}flake.nix{un-bold}:
 
       inputs.std.inputs.${input}.url =
         "${url}";
+  '';
 
-    For reference, see current inputs to Standard
-    in the above trace.
-    ===============================================
+  inputs' = let
+    names = l.attrNames (l.removeAttrs inputs ["self" "cells" "blank"]);
+    nameLengths = map l.stringLength names;
+    maxNameLength =
+      l.foldl'
+      (max: v:
+        if v > max
+        then v
+        else max)
+      0
+      nameLengths;
 
-    🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥 🔥
+    lines =
+      l.map (
+        name: "- ${name}${
+          if isBlank name
+          then pad (maxNameLength - (l.stringLength name)) " | blanked out"
+          else ""
+        }"
+      )
+      names;
+  in
+    "Declared Inputs:\n" + (l.concatStringsSep "\n" lines);
+in
+  assert l.assertMsg (! (isBlank input)) (warn ''
 
-
-
+    ─────┬─────────────────────────────────────────────────────────────────────────
+      🏗️  │ {bold}Input Overloading for ${target}{un-bold}
+    ─────┼─────────────────────────────────────────────────────────────────────────
+      📝 │ {italic}${indent body}{un-italic}
+    ─────┼─────────────────────────────────────────────────────────────────────────
+      🙋 │ ${indent inputs'}
+    ─────┴─────────────────────────────────────────────────────────────────────────
   ''); inputs
