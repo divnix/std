33e2d6a9d3a4e0b88dc4d315bb473c8ddc64a47f
Timothy DeHerrera <tim@nrdxp.dev>
2023-02-26 10:54:05 -0700

    feat: use CI meta to simplify container proviso

--- src/blocktypes/containers.nix
+++ src/blocktypes/containers.nix
@@ -10,9 +10,8 @@
 
   Available actions:
     - print-image
-    - copy-to-registry
-    - copy-to-podman
-    - copy-to-docker
+    - publish
+    - load
   */
   containers = name: {
     __functor = import ./__functor.nix;
@@ -26,32 +25,28 @@
     }: let
       inherit (n2c.packages.${currentSystem}) skopeo-nix2container;
       img = builtins.unsafeDiscardStringContext target.imageName;
-      tags = let
-        tags' = target.meta.tags or [(builtins.unsafeDiscardStringContext target.imageTag)];
-      in
-        builtins.toFile "${target.name}-tags.json" (builtins.unsafeDiscardStringContext (builtins.concatStringsSep "\n" tags'));
-      copyTags = let
+      tags = target.meta.tags or [(builtins.unsafeDiscardStringContext target.imageTag)];
+      tags' =
+        builtins.toFile "${target.name}-tags.json" (builtins.unsafeDiscardStringContext (builtins.concatStringsSep "\n" tags));
+      copyFn = let
         skopeo = "skopeo --insecure-policy";
       in ''
         export PATH=${skopeo-nix2container}/bin:$PATH
 
-        copy_tags() {
-          local name tags source img
-          source=nix:${target}
-          tags=${tags}
-          name=$1
+        copy() {
+          local uri prev_tag
+          uri=$1
           shift
 
-          for tag in $(<$tags); do
-            img="$name:$tag"
-            if ! [[ -v prev_img ]]; then
-              ${skopeo} copy $source "$img" "$@"
+          for tag in $(<${tags'}); do
+            if ! [[ -v prev_tag ]]; then
+              ${skopeo} copy nix:${target} "$uri:tag" "$@"
             else
               # speedup: copy from the previous tag to avoid superflous network bandwidth
-              ${skopeo} copy "$prev_img" "$img" "$@"
+              ${skopeo} copy "$uri:$prev_tag" "$uri:tag" "$@"
             fi
 
-            prev_img="$img"
+            prev_tag="$tag"
           done
         }
       '';
@@ -59,81 +54,39 @@
       (sharedActions.build currentSystem target)
       (mkCommand currentSystem {
         name = "print-image";
-        description = "print out the image name & tag";
+        description = "print out the image name with all tags";
         command = ''
           echo
-          echo "${target.imageName}:${target.imageTag}"
+          for tag in $(<${tags'}); do
+            echo "${img}:$tag"
+          done
         '';
       })
       (mkCommand currentSystem {
         name = "publish";
         description = "copy the image to its remote registry";
         command = ''
-          ${copyTags}
-
-          copy_tags docker://${img}
+          ${copyFn}
+          copy docker://${img}
         '';
-        proviso =
-          l.toFile "container-proviso"
-          # bash
-          ''
-            function proviso() {
-            local -n input=$1
-            local -n output=$2
-
-            local -a images
-            local delim="$RANDOM"
-
-            function get_images () {
-              command nix show-derivation $@ \
-              | command jq -r '.[].env.__provisory'
-            }
-
-            drvs="$(command jq -r '.actionDrv | select(. != "null")' <<< "''${input[@]}")"
-
-            mapfile -t images < <(get_images $drvs)
-
-            command cat << "$delim" > /tmp/check.sh
-            #!/usr/bin/env bash
-            if ! command skopeo inspect --insecure-policy "$1" &>/dev/null; then
-            echo "$1" >> /tmp/no_exist
-            fi
-            $delim
-
-            chmod +x /tmp/check.sh
-
-            rm -f /tmp/no_exist
-
-            echo "''${images[@]}" \
-            | command xargs -n 1 -P 0 /tmp/check.sh
-
-            declare -a filtered
-
-            for i in "''${!images[@]}"; do
-              if command grep "''${images[$i]}" /tmp/no_exist &>/dev/null; then
-                filtered+=("''${input[$i]}")
-              fi
-            done
-
-            output=$(command jq -cs '. += $p' --argjson p "$output" <<< "''${filtered[@]}")
-            }
-          '';
-
-        provisory = target.meta.provisory or "${img}:${target.imageTag}";
+        proviso = l.toFile "container-proviso" ''
+          SKOPEO_INSPECT=${./proviso/skopeo-inspect.sh}
+          ${builtins.readFile ./proviso/publish.sh}
+        '';
+        meta.images = map (tag: "${img}:${tag}") tags;
       })
       (mkCommand currentSystem {
         name = "load";
         description = "load image to the local docker daemon";
         command = ''
-          ${copyTags}
-
+          ${copyFn}
           if command -v podman &> /dev/null; then
-             ixecontainerho "Podman detected: copy to local podman"
-             copy_tags containers-storage:${img} "$@"
+             echo "Podman detected: copy to local podman"
+             copy containers-storage:${img} "$@"
           fi
           if command -v docker &> /dev/null; then
              echo "Docker detected: copy to local docker"
-             copy_tags docker-daemon:${img} "$@"
+             copy docker-daemon:${img} "$@"
           fi
         '';
       })
