4c571a922d79d2849e82bc77202986e1ee920329
David Arnold <david.arnold@iohk.io>
2023-06-20 17:31:40 -0500

    feat: check for PRJ spec on all actions

--- lib/_mkCommand.nix
+++ lib/_mkCommand.nix
@@ -4,5 +4,18 @@ in
   args
   // {
     inherit name description;
-    command = pkgs.writeShellScript "${name}" command;
+    command =
+      pkgs.writeShellScript "${name}" (''
+        set -e
+
+        if test -z "$PRJ_ROOT"; then
+          echo "All Standard Block Type Actions require an environment that fulfills the PRJ Base Directiory Specification"
+          echo "see: https://github.com/numtide/prj-spec"
+          echo "Tip: To achieve that, you can enter a Standard direnv environment or run the action via the Standard CLI/TUI"
+          exit 1
+        fi
+
+        # Action Code follows ...
+      ''
+      + command);
   }
