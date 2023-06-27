e4b9ae73bda29a90f3332bcccf2fc687306f1e6e
David Arnold <david.arnold@iohk.io>
2023-06-27 16:10:39 -0500

    feat: simplify proviso

--- src/std/fwlib/actions/build-filter.jq
+++
@@ -1,5 +0,0 @@
-. as $inputs |
-
-$inputs | map( select(
-  .targetDrv | inside($uncachedDrvs)
-))
--- src/std/fwlib/actions/build-proviso.sh
+++ src/std/fwlib/actions/build-proviso.sh
@@ -1,35 +1,47 @@
-function getUncachedDrvs {
-  local -a uncached
-  local drv
-  drv=$1
+declare action="$1"
+declare targetDrv
 
-  mapfile -t uncached < <(
-    command nix-store --realise --dry-run "$drv" 2>&1 1>/dev/null \
-    | command sed -nrf @extractor@
-  )
+eval "$(jq -r '@sh "targetDrv=\(.targetDrv)"' <<< "$action" )"
 
-  test ''${#uncached[@]} -eq 0 && return;
+mapfile -t uncached < <(
+  command nix-store --realise --dry-run "$targetDrv" 2>&1 1>/dev/null \
+  | command sed -nr '
+    # If the line "will be built" is matched ...
+    /will be built/ {
+        # Create a label to iterate over dervivations
+        :b
+    
+        # Get next line from input into pattern buffer
+        # (Overwrite the pattern buffer)
+        n
+    
+        # If the line matches a nix store path ...
+        /\/nix/ {
+    
+            # ... strip leading whitespaces and print it
+            s/\s*(.*)/\1/p
+    
+            # and go on with the next line (step back to b)     
+            bb
+        }
+    }
+  '
+)
 
-  if (
-     command nix show-derivation ''${uncached[@]} 2> /dev/null \
-     | command jq --exit-status \
-     ' with_entries(
-         select(.value|.env.preferLocalBuild != "1")
-       ) | any
-     ' 1> /dev/null
-  ); then
-    echo "$drv"
-  fi
-}
+if [[ ${#uncached[@]} -eq 0 ]];
+then
+  exit 1
+fi
 
-export -f getUncachedDrvs
+if ! (
+  command nix show-derivation ''${uncached[@]} 2> /dev/null \
+  | command jq --exit-status \
+  ' with_entries(
+      select(.value|.env.preferLocalBuild != "1")
+    ) | any
+  ' 1> /dev/null
+); then
+  exit 1
+fi
 
-command jq --raw-output \
-  --from-file @filter@ \
-  --arg uncachedDrvs "$(
-    parallel -j0 getUncachedDrvs ::: "$(
-       command jq --raw-output 'map(.targetDrv|strings)[]' <<< "$1"
-    )"
-  )" <<< "$1"
-
-unset -f getUncachedDrvs
+exit 0
