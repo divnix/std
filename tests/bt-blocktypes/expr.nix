a1ef840141a2381dced3c7e1c3c94ef7c384724e
David Arnold <david.arnold@iohk.io>
2023-06-29 22:38:18 -0500

    tests: make bt-blocktypes snapshot test less flaky

--- tests/bt-blocktypes/expr.nix
+++ tests/bt-blocktypes/expr.nix
@@ -2,44 +2,52 @@
   inputs,
   std,
   flake-parts,
-}:
-builtins.mapAttrs (
-  n: f: let
-    action = builtins.removeAttrs ({
-        terra = f n "myrepo";
-      }
-      .${n}
-      or (f n)) ["__functor"];
-    buildable = {drvPath = "drvPath";};
-    targets = {
-      runnables = buildable;
-      installables = buildable;
-      devshells = buildable;
-      containers =
-        buildable
-        // {
-          image = {
-            name = "repo:tag";
-            repo = "repo";
-            tag = "tag";
-            tags = ["tag" "tag2"];
+  nixpkgs,
+}: let
+  inherit (builtins) mapAttrs concatStringsSep;
+  inherit (nixpkgs.lib) splitString drop;
+  trimProvisoPath = a:
+    if a ? proviso
+    then a // {proviso = concatStringsSep "/" (drop 4 (splitString "/" a.proviso));}
+    else a;
+in
+  mapAttrs (
+    n: f: let
+      action = builtins.removeAttrs ({
+          terra = f n "myrepo";
+        }
+        .${n}
+        or (f n)) ["__functor"];
+      buildable = {drvPath = "drvPath";};
+      targets = {
+        runnables = buildable;
+        installables = buildable;
+        devshells = buildable;
+        containers =
+          buildable
+          // {
+            image = {
+              name = "repo:tag";
+              repo = "repo";
+              tag = "tag";
+              tags = ["tag" "tag2"];
+            };
           };
-        };
-    };
-  in (
-    if action ? actions
-    then
-      action
-      // {
-        actions = action.actions {
-          inherit inputs;
-          currentSystem = inputs.nixpkgs.system;
-          fragment = "f.r.a.g.m.e.n.t";
-          fragmentRelPath = "x86/f/r/a/g/m/e/n/t";
-          target = targets.${n} or {};
-        };
-      }
-    else action
+      };
+    in (
+      if action ? actions
+      then
+        action
+        // {
+          actions = map trimProvisoPath (action.actions {
+            inherit inputs;
+            currentSystem = inputs.nixpkgs.system;
+            fragment = "f.r.a.g.m.e.n.t";
+            fragmentRelPath = "x86/f/r/a/g/m/e/n/t";
+            target = targets.${n} or {};
+          });
+        }
+      else action
+    )
   )
-)
-std.blockTypes
+  std.blockTypes
