66ae24b6086d56d535dc45df895a4773a3ccf0a6
pegasust <pegasucksgg@gmail.com>
2023-09-02 16:05:21 -0700

    feat(blocktypes/namaka): allow overriding of subdir This allows you to put in {snapshots = {meta.description="your desc"; check = namaka.lib.load {...}; snap-dir = "nix";} This is so that if your repo wants to have everything nix (apart from flake.nix) to be in nix/ or contrib/, maybe via some kind of code ownership system, you can have these snapshots inside

--- src/std/fwlib/blockTypes/namaka.nix
+++ src/std/fwlib/blockTypes/namaka.nix
@@ -17,18 +17,19 @@ in
       inputs,
     }: let
       pkg = inputs.namaka.packages.${currentSystem}.default;
+      subdir = target.snap-dir or "";
     in [
       (mkCommand currentSystem "eval" "use transparently with namaka cli" [] ''
         nix eval '.#${fragment}.check'
       '' {})
       (mkCommand currentSystem "check" "run namaka tests against snapshots" [pkg] ''
-        namaka check -c nix eval '.#${fragment}.check'
+        namaka ${subdir} check -c nix eval '.#${fragment}.check'
       '' {})
       (mkCommand currentSystem "review" "review pending namaka checks" [pkg] ''
-        namaka review -c nix eval '.#${fragment}.check'
+        namaka ${subdir} review -c nix eval '.#${fragment}.check'
       '' {})
       (mkCommand currentSystem "clean" "clean up pending namaka checks" [pkg] ''
-        namaka clean -c nix eval '.#${fragment}.check'
+        namaka ${subdir} clean -c nix eval '.#${fragment}.check'
       '' {})
     ];
   }
