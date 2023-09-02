b7ecce41d2475afa60a0df91b196cb7eb60ff1e7
pegasust <pegasucksgg@gmail.com>
2023-09-02 16:11:20 -0700

    fix: namaka doesn't ignore or accept empty string

--- src/std/fwlib/blockTypes/namaka.nix
+++ src/std/fwlib/blockTypes/namaka.nix
@@ -17,19 +17,22 @@ in
       inputs,
     }: let
       pkg = inputs.namaka.packages.${currentSystem}.default;
-      subdir = target.snap-dir or "";
+      subdir =
+        if target ? "snap-dir"
+        then "${target.snap-dir}"
+        else "";
     in [
       (mkCommand currentSystem "eval" "use transparently with namaka cli" [] ''
         nix eval '.#${fragment}.check'
       '' {})
       (mkCommand currentSystem "check" "run namaka tests against snapshots" [pkg] ''
-        namaka "${subdir}" check -c nix eval '.#${fragment}.check'
+        namaka ${subdir} check -c nix eval '.#${fragment}.check'
       '' {})
       (mkCommand currentSystem "review" "review pending namaka checks" [pkg] ''
-        namaka "${subdir}" review -c nix eval '.#${fragment}.check'
+        namaka ${subdir} review -c nix eval '.#${fragment}.check'
       '' {})
       (mkCommand currentSystem "clean" "clean up pending namaka checks" [pkg] ''
-        namaka "${subdir}" clean -c nix eval '.#${fragment}.check'
+        namaka ${subdir} clean -c nix eval '.#${fragment}.check'
       '' {})
     ];
   }
