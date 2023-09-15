20332712304177602ef232786f21c6c010ebcc40
David Arnold <david.arnold@iohk.io>
2023-09-15 17:34:24 +0200

    refactor: use findTargets for data.configs

--- src/data/configs/default.nix
+++ src/data/configs/default.nix
@@ -2,18 +2,14 @@
   inputs,
   cell,
 }: let
-  inherit (inputs) nixpkgs;
-  inherit (inputs.mdbook-paisano-preprocessor.app.package) mdbook-paisano-preprocessor;
-  inherit (inputs.cells.lib) cfg;
+  inherit (inputs) cells;
+  inherit (inputs.std) findTargets;
 
-  inherit (inputs.nixpkgs.lib) recursiveUpdate;
-in {
-  adrgen = recursiveUpdate cfg.adrgen (import ./adrgen.nix);
-  editorconfig = recursiveUpdate cfg.editorconfig (import ./editorconfig.nix);
-  conform = recursiveUpdate cfg.conform (import ./conform.nix);
-  lefthook = recursiveUpdate cfg.lefthook (import ./lefthook.nix);
-  mdbook = recursiveUpdate cfg.mdbook (scopedImport {inherit inputs;} ./mdbook.nix);
-  treefmt = recursiveUpdate cfg.treefmt (scopedImport {inherit inputs;} ./treefmt.nix);
-  githubsettings = recursiveUpdate cfg.githubsettings (import ./githubsettings.nix);
-  cog = recursiveUpdate cfg.cog (import ./cog.nix);
-}
+  inherit (inputs.nixpkgs.lib) recursiveUpdate mapAttrs;
+
+  data = findTargets {
+    inherit inputs cell;
+    block = ./.;
+  };
+in
+  mapAttrs (name: config: recursiveUpdate config (data.${name} or {})) cells.lib.cfg
