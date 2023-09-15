48ff8cfb435d6514117aeb617dde1969122ac4c8
David Arnold <david.arnold@iohk.io>
2023-09-15 15:30:12 +0200

    refactor: use local config data sets

--- src/data/flake.nix
+++ src/data/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     # injected (private) inputs
-    paisano-mdbook-preprocessor.url = "github:paisano-nix/mdbook-paisano-preprocessor";
-    paisano-mdbook-preprocessor.inputs.nixpkgs.follows = "nixpkgs";
-    paisano-mdbook-preprocessor.inputs.nixago.follows = "";
-    paisano-mdbook-preprocessor.inputs.devshell.follows = "";
+    mdbook-paisano-preprocessor.url = "github:paisano-nix/mdbook-paisano-preprocessor";
+    mdbook-paisano-preprocessor.inputs.nixpkgs.follows = "nixpkgs";
+    mdbook-paisano-preprocessor.inputs.nixago.follows = "";
+    mdbook-paisano-preprocessor.inputs.devshell.follows = "";
 
     # injected inputs to override std's defaults
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
