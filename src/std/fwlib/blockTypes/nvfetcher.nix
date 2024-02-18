d3873fc31dea4c5993e42b9924bb527cb29102f4
David Arnold <dgx.arnold@gmail.com>
2024-02-18 15:25:13 +0100

    feat: add nvfetcher blocktype

---
+++ src/std/fwlib/blockTypes/nvfetcher.nix
@@ -0,0 +1,47 @@
+{
+  root,
+  super,
+}:
+/*
+Use the nvfetcher Blocktype in order to instrucement sources
+with nvfetcher. See its docs for more details.
+
+Available actions:
+  - fetch
+*/
+let
+  inherit (root) mkCommand actions;
+  inherit (super) addSelectorFunctor;
+in
+  name: {
+    __functor = addSelectorFunctor;
+    inherit name;
+    type = "nvfetcher";
+    actions = {
+      currentSystem,
+      fragment,
+      fragmentRelPath,
+      target,
+      inputs,
+    }: let
+      pkgs = inputs.nixpkgs.${currentSystem};
+      inherit (pkgs) lib;
+      inherit (pkgs.stdenv) isLinux;
+    in [
+      (mkCommand currentSystem "fetch" "update source" [pkgs.nvfetcher] ''
+         targetname="$(basename ${fragmentRelPath})"
+         blockpath="$(dirname ${fragmentRelPath})"
+         cellpath="$(dirname "$blockpath")"
+         tmpfile="$(mktemp)"
+         updates="$PRJ_ROOT/${fragmentRelPath}.md"
+         nvfetcher \
+           --config "$PRJ_ROOT/$cellpath/nvfetcher.toml" \
+           --build-dir "$PRJ_ROOT/$blockpath" \
+           --changelog "$tmpfile" \
+           --filter "^$targetname$"
+
+        sed -i -e "s|^|- \`$(date --iso-8601=m)\` |" "$tmpfile"
+        cat "$tmpfile" >> "$updates"
+      '' {})
+    ];
+  }
