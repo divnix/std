f1a6bc4972162ca1ea21f7d281896bce619626e1
David <dgx.arnold@gmail.com>
2025-08-25 17:41:29 +0200

    fix: use a shallow clone in CI

--- .github/workflows/update-subflake.sh
+++ .github/workflows/update-subflake.sh
@@ -2,5 +2,5 @@
 nix store add-path --name source .
 
 # This locks subflakes to the latest commit of std
-(cd ./src/local && nix flake lock --override-input std git+file://$(pwd)/../..?rev=$(git -C ../.. rev-parse HEAD))
-(cd ./src/tests && nix flake lock --override-input std git+file://$(pwd)/../..?rev=$(git -C ../.. rev-parse HEAD))
+(cd ./src/local && nix flake lock --override-input std git+file://$(pwd)/../..?rev=$(git -C ../.. rev-parse HEAD)&shallow=1)
+(cd ./src/tests && nix flake lock --override-input std git+file://$(pwd)/../..?rev=$(git -C ../.. rev-parse HEAD)&shallow=1)
